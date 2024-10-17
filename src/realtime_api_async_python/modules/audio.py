e805429bf6ad9d4cf4f21bdf185320631639a748
qwukker <minor7addfloortom@gmail.com>
2024-10-17 16:59:23 -0500

    progress

--- src/realtime_api_async_python/modules/audio.py
+++ src/realtime_api_async_python/modules/audio.py
@@ -3,15 +3,18 @@ import pyaudio
 import logging
 from .utils import FORMAT, CHANNELS, RATE
 
+
 async def play_audio(audio_data):
     p = pyaudio.PyAudio()
     stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True)
     stream.write(audio_data)
 
-    # Add a small delay (e.g., 100ms) of silence at the end to prevent popping, and weird cuts off sounds
-    silence_duration = 0.2  # 200ms
+    # Add a small delay of silence at the end to prevent popping, and weird cuts off sounds
+    silence_duration = 0.4
     silence_frames = int(RATE * silence_duration)
-    silence = b"\x00" * (silence_frames * CHANNELS * 2)  # 2 bytes per sample for 16-bit audio
+    silence = b"\x00" * (
+        silence_frames * CHANNELS * 2
+    )  # 2 bytes per sample for 16-bit audio
     stream.write(silence)
 
     # Add a small pause before closing the stream to make sure the audio is fully played
